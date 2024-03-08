32adde5ff27977025665d5658959a1375230acb5
AaryanDahal17 <malleckmelon@gmail.com>
2024-03-08 21:37:31 +0545

    some bot updates

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # sudoku-bot-using-cpp
+
+## To Run , type the following in your terminal : 
+
+```bash
+g++ bot.cpp
+./a.exe
+```
+
+Enter sudoku gameState row by row in the following format :
+
+1 9 0 2 3 8 0 0 7   (0 represents blank space)
+
+The solved board will be printed (if the input is valid) 
+
+Enjoy
--- bot.cpp
+++ bot.cpp
@@ -4,56 +4,112 @@
 using namespace std;
 
 
-struct node{
 
-node* parentNode;
-};
-
-
-void inputGameState(int arr[9][9]){
-
+void inputSudokuBoard(int board[9][9]) {
     cout << "Enter input of the row in the format -> 0 0 1 9 2 5 0 0 0 " << endl;
 
-    // Getting the gameState from user
+    for (int i = 0; i < 9; i++) {
+        cout << "Enter row no. " << i + 1 << " : ";
 
-    for (int i = 0; i < 9 ; i++){
+        std::string userInput;
+        std::getline(std::cin, userInput);
 
-    cout << "Enter row no. " << i+1 << " : ";
-       
-    std::string userInput;
+        std::stringstream ss(userInput);
 
-    std::getline(std::cin, userInput);
+        int number, index = 0;
 
-    std::stringstream ss(userInput);
-
-    int num, index = 0;
-
-    while (ss >> num && index < 9) {
-        if(num > 9  || num < 0){
-            cout << "Invalid input" << endl;
-            exit(0);
+        while (ss >> number && index < 9) {
+            if (number > 9 || number < 0) {
+                cout << "Invalid input" << endl;
+                exit(0);
+            }
+            board[i][index++] = number;
         }
-       arr[i][index++] = num;
+
+        cout << endl;
     }
-
-    cout << endl;
-
-    }
-
 }
 
+const int SIZE = 9;
 
-int main(){
+bool isValidPlacement(int sudokuBoard[][SIZE], int row, int col, int num) {
+    for (int i = 0; i < SIZE; i++) {
+        if (sudokuBoard[row][i] == num || sudokuBoard[i][col] == num) {
+            return false;
+        }
+    }
 
+    int boxStartRow = row - row % 3;
+    int boxStartCol = col - col % 3;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (sudokuBoard[boxStartRow + i][boxStartCol + j] == num) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool findEmptyCell(int sudokuBoard[][SIZE], int& row, int& col) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (sudokuBoard[i][j] == 0) {
+                row = i;
+                col = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool solveSudokuBoard(int sudokuBoard[][SIZE]) {
+    int row, col;
+    if (!findEmptyCell(sudokuBoard, row, col)) {
+        return true;
+    }
+
+    for (int num = 1; num <= SIZE; num++) {
+        if (isValidPlacement(sudokuBoard, row, col, num)) {
+            sudokuBoard[row][col] = num;
+
+            if (solveSudokuBoard(sudokuBoard)) {
+                return true;
+            }
+
+            sudokuBoard[row][col] = 0; // Backtrack
+        }
+    }
+
+    return false;
+}
+
+void printSudokuBoard(int sudokuBoard[][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            cout << sudokuBoard[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
     
-    int gameState[9][9];
+    int sudokuBoardState[SIZE][SIZE];
+    inputSudokuBoard(sudokuBoardState);
 
-    inputGameState(gameState);
-    
+    if (solveSudokuBoard(sudokuBoardState)) {
 
-   
+        cout << "Solved Sudoku Board:" << endl;
 
+        printSudokuBoard(sudokuBoardState);
+    } else {
 
+        cout << "No solution exists for this Sudoku Board.";
+    }
 
     return 0;
 }
